b7385040fed27d2e52f473e39aa3d36f776913da
Bookswallah <108852307+Bookswallah@users.noreply.github.com>
2022-07-07 20:55:04 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,10 +1,14 @@
 class Presets(object):
-    WELCOME_MESSAGE = """
-<b>Hello.. </b>{}
-<code>I'm an official telecaster bot
-maintained by</code> <b>{}</b>
-<code>Keep me unblocked to receive ma official updates.</code>
-    """
+    WELCOME_MESSAGE = ""
+<b>Dear.. </b>{}
+<code>I will update you always about latest suff.
+powered by</code> <b>{}</b>
+<code>Keep me pinned in your telegram </code>
+
+Self coded bot by Books wallah 
+
+Growing Future of India🇮🇳
+    ""
     USERS_LIST = "<b>Total:</b>\n\nSubscribers - {}\nBlocked / Deleted - {}"
     WAIT_MSG = "<b>Please Wait...</b>"
     REPLY_ERROR = "<code>Use this command as a reply to any telegram message with out any spaces.</code>"
